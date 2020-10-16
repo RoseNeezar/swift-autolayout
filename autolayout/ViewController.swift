35b3f847d8cd46978c80cbcb735ee93fdcbfd15d
Neezar <rforlyffe@gmaicl.com>
2020-10-16 12:37:04 +0800

    init commit

--- autolayout/ViewController.swift
+++ autolayout/ViewController.swift
@@ -12,6 +12,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let practiceView = UIView()
+        practiceView.backgroundColor = UIColor.red
+        
+        view.addSubview(practiceView)
+        
+        practiceView.frame = CGRect(x: 0, y: 0, width: 375, height: 200)
     }
 
 
